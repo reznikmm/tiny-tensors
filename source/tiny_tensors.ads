010c5702cc0b9fd663d923d4ed820b2d1eef477e
Maxim Reznik <reznikmm@gmail.com>
2025-11-03 11:55:28 +0200

    Add initial files

---
+++ source/tiny_tensors.ads
@@ -0,0 +1,9 @@
+--  SPDX-FileCopyrightText: 2025 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+package Tiny_Tensors is
+   pragma Pure;
+
+end Tiny_Tensors;
